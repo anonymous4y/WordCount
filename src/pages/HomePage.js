3791acae45b27b913229407322c67c0cae8e45fa
Harsh Kumar <harshsharma1481@gmail.com>
2023-07-17 20:17:32 +0530

    word count

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom"
 import { Word } from "../components"
 
 export const HomePage = () => {
     return (
         <main>
             <Word />
+            <div className="flex justify-center my-10">
+                <Link to='/joke'>
+                    <button type="button" class="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-bold rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Free Jokes</button>
+                </Link>
+            </div>
         </main>
     )
 }
